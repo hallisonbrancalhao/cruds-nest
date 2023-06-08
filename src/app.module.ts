9a5d7688ee9f4f47eb8af3eebf1012538127af3b
Hallison Brancalhão <hallison.o.b@gmail.com>
2023-06-08 04:44:27 -0300

    feat:(user-repository) modules providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,36 @@
-import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
-import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { UserRepository } from './users/repository/user.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://0.0.0.0/nest'), UsersModule, AuthModule, ProductsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://0.0.0.0/nest'),
+    UsersModule,
+    AuthModule,
+    ProductsModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: UserRepository,
+      useClass: UsersService,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes({ path: 'users', method: RequestMethod.GET });
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'users', method: RequestMethod.GET });
   }
 }
